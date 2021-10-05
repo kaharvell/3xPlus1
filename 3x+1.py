3ca5576fddd8a2e9f26574defa751e72b86996e4
kaharvell <62230854+kaharvell@users.noreply.github.com>
2021-10-05 08:41:25 -0600

    Add files via upload

--- 3x+1.py
+++ 3x+1.py
@@ -7,17 +7,19 @@ def main():
     file = 'db.txt'
     i = 0
 
+    # assign 'starting num' in file to iterator
     if os.path.exists(file):
         with open(file) as f:
             entries = f.readlines()
             i = int(entries[-1][14:]) + 1
             print(i)
+    # create file if file does not exist
     else:
         with open(file, 'x') as f:
             f.writelines('num, count, positive polarity count, negative polarity count, summation, average, variance, standard deviation\n')
             f.writelines(f'starting num: {i}')
 
-    while i > -1:
+    while i < 1000000001:
         occurrence = []
         num = i
         pos_polarity_count = 0
@@ -26,9 +28,11 @@ def main():
         while num not in occurrence:
             occurrence.append(num)
 
+            # if num is even divide by two
             if num % 2 == 0:
                 pos_polarity_count += 1
                 num = num>>1
+            # else multiply by three add one
             else:
                 neg_polarity_count += 1
                 num = (num<<1) + num + 1
@@ -36,33 +40,36 @@ def main():
         count = len(occurrence)
         replace = f'starting num: {i + 1}'
 
+        # store file contents to a list to rewrite file using write_to_file()
+        with open(file, 'r') as f:
+                db = f.readlines()
+
+        # if sequence length is greater than max, write variables to file
         if count > max:
             max = count
             summation = sum(occurrence)
             avg = summation/count
             variance = numpy.var(occurrence)
             deviation = numpy.std(occurrence)
+            s = f'{i}, {count}, {pos_polarity_count}, {neg_polarity_count}, {summation}, {avg}, {variance}, {deviation}\n'
 
-            with open(file, 'r') as f:
-                db = f.readlines()
-                with open(file, 'w') as f:
-                    for item in db[:-1]:
-                        f.write(item)
-                    f.writelines(f'{i}, {count}, {pos_polarity_count}, {neg_polarity_count}, {summation}, {avg}, {variance}, {deviation}\n')
-                    f.write(replace)
+            write_to_file(file, db, replace, s)
+
+        # else write last num calculated to file to be assigned to iterator when program resumes
         else:
-            with open(file, 'r') as f:
-                db = f.readlines()
-                with open(file, 'w') as f:
-                    for item in db[:-1]:
-                        f.write(item)
-                    f.write(replace)
-
+            write_to_file(file, db, replace)
 
         i += 1
 
         print(i)
 
+def write_to_file(file, content, replace, s = ''):
+    with open(file, 'w') as f:
+                for item in content[:-1]:
+                    f.write(item)
+                if len(s) != 0:
+                    f.writelines(s)
+                f.write(replace)
 
 if __name__== "__main__":
    main()
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-"# 3xPlus1" 
+#3xPlus1
+
+In the chase of infinity, a number which can not be extrapolated by its very nature, 3xPlus1 aims to create a public database of observations of each maximum in ascending order.
+
+##What Is 3x+1?
+
+3x+1, or the **Collatz conjecture**, takes a number, and if it is odd, multiplies by three, else divides by two. Pretty simple, right? Staticians and mathmeticians who are passionate about the infinite are led to believe that there is a number that could be multiplied by three plus one all the way to infinity, meaning it never stops, or even simplier put, never reaches an even number. When it does reach an even number, the halving always ends in a violent spiral to an infinite loop of 4, 2, 1, 4. If you're interested in learning more about 3x+1, more information can be found [here](https://en.wikipedia.org/wiki/Collatz_conjecture).
+
+##What Does The Program Do?
+
+3xPlus1 does one thing currently, it stores information about each number, such as:
+
+- The current maximum sequence length (all the numbers an iteration 'reaches' before returning to 4, 2, 1).
+- Positive and negative 'polarity count' (count of positive and negative numbers, each).
+- The sum of all numbers in an iterations sequence.
+- The average of all numbers in an iterations sequence.
+- The variance of the sequence.
+- The standard deviation of the sequence.
+
+Currently, that is all it does and all it needs to do. I am aggregating this data to further analyze it, as I believe there could be a correlation or pattern that could lead to an exponential formula to better 'estimate' the next highest number based on the data above.
+
+## Future implementation
+
+There is a lot of potential for add-on, so if you have any suggestions, please feel free to email me at kaharvell@gmail.com!
